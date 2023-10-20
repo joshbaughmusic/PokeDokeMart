4af27a8f0005d1c2c4d90e26e0cedb72e9c9427f
Josh Baugh <joshbaughmusic@gmail.com>
2023-10-20 13:19:28 -0500

    checkout view done

---
+++ client/src/components/checkout/Checkout.js
@@ -0,0 +1,70 @@
+import {
+  Spinner,
+  ListGroup,
+  ListGroupItem,
+  Input,
+  Container,
+} from 'reactstrap';
+import { useShoppingCart } from '../../context/ShoppingCartContext.js';
+import { AiOutlineClose } from 'react-icons/ai';
+import { CheckoutForm } from './CheckoutForm.js';
+
+export const Checkout = () => {
+  const { cartItems, alterCartQuantity, removeFromCart } = useShoppingCart();
+
+  if (!cartItems) {
+    return <Spinner />;
+  }
+
+  return (
+    <Container>
+      <h2 className="mt-3">Checkout</h2>
+      <ListGroup className="mt-4">
+        {cartItems.length > 0 ? (
+          cartItems.map((item, index) => {
+            return (
+              <ListGroupItem
+                key={index}
+                className="text-bg-dark d-flex justify-content-between"
+              >
+                <img
+                  src={item.image}
+                  alt=""
+                />
+                <p>{item.name}</p>
+                <p>P{item.cost}</p>
+                <Input
+                  type="number"
+                  min={1}
+                  value={item.quantity}
+                  onChange={(e) => alterCartQuantity(item, parseInt(e.target.value))}
+                  style={{ width: '20%' }}
+                />
+                <AiOutlineClose
+                  style={{
+                    fontSize: '25px',
+                  }}
+                  onClick={() => removeFromCart(item.id)}
+                />
+              </ListGroupItem>
+            );
+          })
+        ) : (
+          <p>Your PokeCart is empty!</p>
+        )}
+      </ListGroup>
+      <div>
+        {cartItems.length > 0 ? (
+          <h5>
+            Total: P
+            {cartItems.reduce(
+              (totalPrice, item) => totalPrice + item.cost * item.quantity,
+              0
+            )}
+          </h5>
+        ) : null}
+        {cartItems.length > 0 ? <CheckoutForm cartItems={cartItems} /> : null}
+      </div>
+    </Container>
+  );
+};
