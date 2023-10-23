c42887211580691b334a91b930d5e08528336d78
Josh Baugh <joshbaughmusic@gmail.com>
2023-10-23 14:38:45 -0500

    edituserdetails done

--- client/src/managers/ProfileManager.js
+++ client/src/managers/ProfileManager.js
@@ -3,3 +3,11 @@ const _apiUrl = '/api/userprofile';
 export const fetchCurrentUserProfile = () => {
   return fetch(`${_apiUrl}/me`).then(res => res.json());
 };
+
+export const fetchUpdateUserProfile = (updatedUserProfile) => {
+    return fetch(`${_apiUrl}/update`, {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(updatedUserProfile)
+    });
+}
