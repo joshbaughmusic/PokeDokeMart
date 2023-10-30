fd9267412410c2f61708f53e39503044c34c75a6
Josh Baugh <joshbaughmusic@gmail.com>
2023-10-30 14:53:26 -0500

    uppper left section done

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,7 +5,8 @@ import { MyOrders } from './MyOrders.js';
 import { EditUserDetails } from './EditUserDetails.js';
 import { ProfileAllReviews } from '../reviews/ProfileAllReviews.js';
 import { MyPokemonList } from '../pokemon/MyPokemonList.js';
-import PersonSprite from '../../images/personsprite.png'
+import PersonSprite from '../../images/personsprite.png';
+import './Profile.css';
 
 export const Profile = ({ loggedInUser }) => {
   const [profile, setProfile] = useState();
@@ -19,71 +20,63 @@ export const Profile = ({ loggedInUser }) => {
   }, []);
 
   if (!profile) {
-    return <Spinner />;
+    return (
+      <>
+        <div className="d-flex justify-content-center h-75 align-items-center">
+          <Spinner />
+        </div>
+      </>
+    );
   }
   return (
     <>
       <Container className="text-bg-dark my-5 p-4">
-        <h1 className="mb-3">
+        <h1 className="profile-heading mb-3">
           {profile.firstName} {profile.lastName}
         </h1>
         <Row>
-          <Col className="d-flex gap-5 align-items-center">
+          <Col className="d-flex gap-4 align-items-center">
             <div>
               {profile.profilePictureUrl ? (
                 <img
+                  className="profile-image"
                   src={profile.profilePictureUrl}
                   alt=""
                   style={{
-                    width: '200px',
-                    height: '200px',
+                    width: '210px',
+                    height: '210px',
                     objectFit: 'cover',
-                    border: 'solid lightgrey 3px',
                   }}
                 />
-                ) : (
+              ) : (
                 <img
+                  className="profile-image"
                   src={PersonSprite}
                   alt=""
                   style={{
                     width: '200px',
                     height: '200px',
                     objectFit: 'cover',
-                    border: 'solid lightgrey 3px',
                   }}
                 />
-                //code for initials if blank pic instead
-                
-                // <div className='d-flex justify-content-center align-items-center'
-                //   alt=""
-                //   style={{
-                //     width: '200px',
-                //     height: '200px',
-                //     objectFit: 'cover',
-                //     border: 'solid lightgrey 3px',
-                //   }}
-                // >
-                //   <h1 className='display-3'>
-                //     {profile.firstName.slice(0, 1)}.
-                //     {profile.lastName.slice(0, 1)}.
-                //   </h1>
-                // </div>
               )}
             </div>
-            <div>
-              <div className="d-flex justify-content-between align-items-baseline">
-                <h5>Details:</h5>
-                <EditUserDetails
-                  profile={profile}
-                  getCurrentUserProfile={getCurrentUserProfile}
-                />
-              </div>
-              <div className="pt-3">
-                <p>{profile.email}</p>
-                <p>{profile.address}</p>
-                <p>
-                  {profile.city.name}, {profile.region.name}
-                </p>
+            <div className="profile-details p-3">
+              <div>
+                <div className="d-flex justify-content-between align-items-baseline">
+                  <h5>Details:</h5>
+                  <EditUserDetails
+                    profile={profile}
+                    getCurrentUserProfile={getCurrentUserProfile}
+                  />
+                </div>
+                <div className="pt-3">
+                  <p>Email: {profile.email}</p>
+                  <p>Street: {profile.address}</p>
+                  <p>
+                    C/R: {profile.city.name}, {profile.region.name}
+                  </p>
+                </div>
               </div>
             </div>
           </Col>
@@ -105,3 +98,20 @@ export const Profile = ({ loggedInUser }) => {
     </>
   );
 };
+
+//code for initials if blank pic instead
+
+// <div className='d-flex justify-content-center align-items-center'
+//   alt=""
+//   style={{
+//     width: '200px',
+//     height: '200px',
+//     objectFit: 'cover',
+//     border: 'solid lightgrey 3px',
+//   }}
+// >
+//   <h1 className='display-3'>
+//     {profile.firstName.slice(0, 1)}.
+//     {profile.lastName.slice(0, 1)}.
+//   </h1>
+// </div>
