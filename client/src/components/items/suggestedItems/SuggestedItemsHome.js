8570c63b94c0b37b4386eaa29c6a60390c4c9d69
Josh Baugh <joshbaughmusic@gmail.com>
2023-10-31 14:48:47 -0500

    fixes

--- client/src/components/items/suggestedItems/SuggestedItemsHome.js
+++ client/src/components/items/suggestedItems/SuggestedItemsHome.js
@@ -3,9 +3,11 @@ import { fetchSuggestedItemsByUser } from '../../../managers/ItemsManager.js';
 import { Spinner } from 'reactstrap';
 import { SuggestedItemsCarouselHome } from './SuggestedItemsCarouselHome.js';
 import PokeballLoading from '../../../images/pokeball-loading.gif';
+import { useNavigate } from 'react-router-dom';
 
 export const SuggestedItemsHome = ({ loggedInUser }) => {
   const [suggestedItems, setSuggestedItems] = useState();
+  const navigate = useNavigate()
 
   const getSuggestedItemsByUser = () => {
     fetchSuggestedItemsByUser(loggedInUser.id, 12).then(setSuggestedItems);
@@ -34,11 +36,13 @@ export const SuggestedItemsHome = ({ loggedInUser }) => {
 
   return (
     <>
-      <h3 className="suggested-items-heading text-center">Suggested items for you:</h3>
+      <h3 className="suggested-items-heading text-center">
+        Suggested items for you:
+      </h3>
       {suggestedItems.length === 0 ? (
-        <div className='text-center mt-5'>
-          Add some pokemon to your profile to receive personalized item
-          suggestions!
+        <div className="text-center mt-5">
+          Add some pokemon to your <span role="button" className='suggested-items-profile-link' onClick={() => navigate("/profile")}>profile</span> to receive personalized
+          item suggestions!
         </div>
       ) : (
         <SuggestedItemsCarouselHome items={suggestedItems} />
